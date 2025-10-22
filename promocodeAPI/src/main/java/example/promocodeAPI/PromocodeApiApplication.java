807d6997374439653bbdde940a0a5a6422adbe53
Muart <shakurovmurat@gmail.com>
2025-10-22 21:43:44 +0500

    Initial commit with project files

---
+++ promocodeAPI/src/main/java/example/promocodeAPI/PromocodeApiApplication.java
@@ -0,0 +1,13 @@
+package example.promocodeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocodeApiApplication.class, args);
+	}
+
+}
